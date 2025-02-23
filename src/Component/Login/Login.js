e539385f945faa6cdf31832127314079030a6695
sanjina <np03cs4a220101@heraldcollege.edu.np>
2025-02-23 13:08:03 +0545

    Integrate backend in the login

--- src/Component/Login/Login.js
+++ src/Component/Login/Login.js
@@ -2,20 +2,76 @@ import React, { useState, useEffect } from "react";
 import { FaEnvelope, FaLock } from "react-icons/fa";
 import "./Login.css";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../../redux/user/userSlice";
 import { toast } from "react-toastify";
 
 function Login() {
+  const { user, loading, error } = useSelector((state) => state.user);
+  console.log(user, "usero");
+  const dispatch = useDispatch();
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
   const navigate = useNavigate();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const userData = {
+      email: email,
+      password: password,
+    };
+
+    try {
+      dispatch(signInStart());
+
+      const response = await fetch("http://localhost:8081/login", {
+        method: "POST",
+        credentials: "include",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(userData),
+      });
+      const data = await response.json();
+
+      console.log(response, "re");
+      console.log(data, "reda");
+      setMessage(data.message);
+
+      if (!response.ok) {
+        toast.error("Login error", {
+          position: "top-right",
+        });
+        dispatch(signInFailure(data.message));
+        return;
+      }
+
+      dispatch(signInSuccess(data));
+      toast.success("LoggedIn Successful", {
+        position: "top-right",
+      });
+
+      if (data.role === "admin") {
+        navigate("/AdminBooking");
+      } else {
+        navigate("/");
+      }
+    } catch (error) {
+      dispatch(signInFailure(error.message));
+    }
+  }
 
   return (
     <div className="main-wrapper">
       <div className="wrapper">
         <h1 className="font-semibold">Login</h1>
         <h2>{message}</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className=" input-box">
             <input
               placeholder="Email"
