5cf94a7c6a1ceca55c2b1745a3e5ac0c3a95a486
sanjina <np03cs4a220101@heraldcollege.edu.np>
2025-03-09 00:09:12 +0545

    Added Utils

---
+++ src/Component/utils/checkAdmin.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import { Route, Navigate } from "react-router-dom";
+
+const AdminVehicleRoute = ({ element, ...rest }) => {
+  const [isAdmin, setIsAdmin] = useState(false);
+  console.log("yers");
+  useEffect(() => {
+    const fetchAdminRole = async () => {
+      try {
+        const response = await fetch("http://localhost:8081/checkAdmin", {
+          method: "GET",
+          credentials: "include",
+        });
+        const data = await response.json();
+        console.log(data.userRole, "dti");
+        if (data.userRole === "user") {
+          setIsAdmin(true);
+        }
+      } catch (error) {
+        console.error("Error fetching admin role:", error);
+      }
+    };
+
+    fetchAdminRole();
+  }, []);
+  return isAdmin ? (
+    <Route {...rest} element={element} />
+  ) : (
+    <Navigate to="/profile" />
+  );
+};
+
+export default AdminVehicleRoute;
