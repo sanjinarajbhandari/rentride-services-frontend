170be05c3b96bd5721de1a963299f8ecd8107a73
sanjina <np03cs4a220101@heraldcollege.edu.np>
2025-03-08 22:17:59 +0545

    Updated AdminNav.js

--- src/Component/Admin/AdminNav.js
+++ src/Component/Admin/AdminNav.js
@@ -1,43 +1,62 @@
 import React, { useState } from "react";
 import { HashLink as Link } from "react-router-hash-link";
-import { FaUser } from "react-icons/fa";
+import { FaArrowRight, FaUser } from "react-icons/fa";
 import "../Navbar/Navbar.css";
+import { useSelector } from "react-redux";
+import Profile from "../profile/profile";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { user } = useSelector((state) => state.user);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
   return (
-    <nav className="NavbarItem">
-      <h1 className="font-bold text-xl">RentRide Services</h1>
-      <div className="MenuIcons" onClick={toggleMenu}>
-        <i className={isMenuOpen ? "fas fa-times" : "fas fa-bars"}></i>
-      </div>
+    <>
+      <nav className="NavbarItem">
+        <h1 className="font-bold text-xl">RentRide Services</h1>
+        <div className="MenuIcons" onClick={toggleMenu}>
+          <i className={isMenuOpen ? "fas fa-times" : "fas fa-bars"}></i>
+        </div>
 
-      <ul className={isMenuOpen ? "Navmenu active" : "Navmenu"}>
-        <li>
-          <Link className="NavLinks" to="/AdminVehicle">
-            <i className="fas fa-car"></i> Vehicle
-          </Link>
-        </li>
-        
-        <li>
-          <Link className="NavLinks" to="/AdminBooking">
-            <i className="fas fa-list"></i> Bookings
-          </Link>
-        </li>
-       
-      </ul>
+        <ul className={isMenuOpen ? "Navmenu active" : "Navmenu"}>
+          <li>
+            <Link className="NavLinks" to="/AdminVehicle">
+              <i className="fas fa-car"></i>Vehicle
+            </Link>
+          </li>
+          {/* <li>
+            <Link className="NavLinks" to="/Admin/Product">
+              <i className="fas fa-bag-shopping"></i>Product
+            </Link>
+          </li> */}
+          <li>
+            <Link className="NavLinks" to="/AdminBooking">
+              <i className="fas fa-list"></i>Bookings
+            </Link>
+          </li>
+          {/* <li>
+            <Link className="NavLinks" to="/AdminCheckout">
+              <i className="fas fa-cart-shopping"></i>Cart
+            </Link>
+          </li> */}
+        </ul>
 
-      <div className="corner-menu">
-        <Link to="/Registration">
-          <FaUser />
-        </Link>
-      </div>
-    </nav>
+        <div className="corner-menu">
+          {user ? (
+            <Link to="/profile">
+              <FaUser />
+            </Link>
+          ) : (
+            <Link to="/Registration">
+              <FaUser />
+            </Link>
+          )}
+        </div>
+      </nav>
+    </>
   );
 };
 
