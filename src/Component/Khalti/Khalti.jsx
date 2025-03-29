5e0fb8bd611962f844fdb72f0806d2775ef0e700
sanjina <np03cs4a220101@heraldcollege.edu.np>
2025-03-30 00:39:05 +0545

    Khalti payment integration

--- src/Component/Khalti/Khalti.jsx
+++ src/Component/Khalti/Khalti.jsx
@@ -2,19 +2,9 @@ import React, { useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 const Khalti = ({ amounto, purpose }) => {
-  console.log(
-    amounto,
-    purpose,
-    "-------------------------------------------------------------------------------"
-  );
   useEffect(() => {
     const paisa = parseInt(amounto) * 100;
-    console.log(
-      paisa,
-      "**************************************************************"
-    );
     const submitAlbum = async () => {
-      console.log("heheheheh");
       try {
         const purchase_order_id = uuidv4();
         const payload = {
@@ -34,10 +24,8 @@ const Khalti = ({ amounto, purpose }) => {
           body: JSON.stringify(payload),
         });
 
-        console.log(response, "ressssssss");
         if (response) {
           const responseData = await response.json();
-          console.log(responseData, "daaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
           window.location.href = `${responseData?.payment_url}`;
         }
       } catch (error) {
